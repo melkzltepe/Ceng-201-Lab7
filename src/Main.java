bdc33e9b04d53f1f521d4606af7825cca1dea085
Melike <melike0921kiziltepe@gmail.com>
2025-11-06 10:24:01 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        int[] array = {1,2,3,4,5,6};
+        System.out.println(countEvenNumbers(array, 0 ));
+    }
 
+    public static int countEvenNumbers(int[] array, int index) {
+        if (index == array.length) {
+            return 0;
+        }
+        if (array[index] % 2 == 0){
+            return (1 + countEvenNumbers(array, index + 1));
+        }else {
+            return (countEvenNumbers(array, index + 1));
+        }
     }
 }
